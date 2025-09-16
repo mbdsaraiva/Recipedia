986fb0ddef1f7913fc169f0d85a6c526f465d420
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-09-16 17:23:37 -0300

    endpoint post users

--- back/server.js
+++ back/server.js
@@ -23,9 +23,20 @@ app.get('/api/users', async(req,res) => {
     catch (error) {
         res.status(500).json({error: "Erro ao buscar usuários"});
     }
-})
+});
 
-
+app.post('/api/users', async(req,res) => {
+    try{
+        const {nome, email, senha, tipo} = req.body;
+        const user = await prisma.user.create({
+            data: {nome, email, senha, tipo}
+        })
+        res.status(201).json(user);
+    }
+    catch (error) {
+        res.status(500).json({erro: 'Erro ao criar usuário'});
+    }
+});
 
 app.listen(3000,() => {
     console.log(`Servidor rodando na porta 3000`)
