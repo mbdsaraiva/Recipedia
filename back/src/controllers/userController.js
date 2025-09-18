344b555a16b5c1c23f9d3e26b5696e349575a0dd
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-09-17 23:08:20 -0300

    refatoracao userController deleteUser e exportacao dos modulos

--- back/src/controllers/userController.js
+++ back/src/controllers/userController.js
@@ -1,60 +1,59 @@
-const {prisma} = require('../config/database')
+const { prisma } = require('../config/database')
 
 // lista todos os usuarios
-async function getAllUsers(req,res){
-    try{
+async function getAllUsers(req, res) {
+    try {
         const users = await prisma.user.findMany();
         res.json(users);
     }
-    catch(error){
+    catch (error) {
         console.error('Erro ao buscar usuarios:', error);
-        res.status(500).json({error:'Erro ao buscar usuarios'});
+        res.status(500).json({ error: 'Erro ao buscar usuarios' });
     }
 }
 
 // lista usuario por id
-async function getUserById(req,res){
-    try{
-        const {id} = req.params;
+async function getUserById(req, res) {
+    try {
+        const { id } = req.params;
         const user = await prisma.user.findUnique({
-            where: {id: parseInt(id)},
+            where: { id: parseInt(id) },
             include: {
                 receitas: true,
-                estoque:{
-                    include:{
+                estoque: {
+                    include: {
                         ingredient: true
                     }
                 }
             }
         });
 
-        if(!user){
-            return res.status(404).json({error: 'Usuario nao encontrado'});
+        if (!user) {
+            return res.status(404).json({ error: 'Usuario nao encontrado' });
         }
 
         res.json(user);
 
     }
-    catch(error){
+    catch (error) {
         console.error('Erro ao buscar usuario', error);
-        res.status(500).json({error: 'Erro ao buscar usuario'});
+        res.status(500).json({ error: 'Erro ao buscar usuario' });
     }
 
 }
 
-async function createUser(req,res){
+async function createUser(req, res) {
+    try {
+        const { nome, email, senha, tipo } = req.body;
 
-    try{
-        const {nome, email, senha, tipo} = req.body;
-
-        if(!nome || !email || !senha){
+        if (!nome || !email || !senha) {
             return res.status(400).json({
                 error: 'Nome, senha e email sao obrigatorios'
             });
         }
-        
+
         const user = await prisma.user.create({
-            data: {nome, email, senha, tipo: tipo || 'user'}
+            data: { nome, email, senha, tipo: tipo || 'user' }
         });
 
         res.status(201).json({
@@ -62,42 +61,41 @@ async function createUser(req,res){
             user
         });
     }
-    catch (error){
+    catch (error) {
         console.error('Erro ao criar usuario', error);
-        if(error.code === 'P2002'){
+        if (error.code === 'P2002') {
             res.status(400).json({
                 error: 'Email ja em uso'
             })
 
-        } else{
-            res.status(500).json({error: 'Erro ao criar o usuario'});
+        } else {
+            res.status(500).json({ error: 'Erro ao criar o usuario' });
         }
     }
 }
 
-async function updateUser(req,res){
-
-    try{
-        const {id} = req.params;
-        const {nome, email, senha, tipo} = req.body;
+async function updateUser(req, res) {
+    try {
+        const { id } = req.params;
+        const { nome, email, senha, tipo } = req.body;
 
         const userExists = await prisma.user.findUnique({
-            where: {id: parseInt(id)}
+            where: { id: parseInt(id) }
         });
 
-        if(!userExists){
-            return res.status(404).json({error: 'Usuario nao encontrado'});
+        if (!userExists) {
+            return res.status(404).json({ error: 'Usuario nao encontrado' });
         }
 
         const updateData = {};
 
-        if(nome) updateData.nome = nome;
-        if(email) updateData.email = email;
-        if(senha) updateData.senha = senha;
-        if(tipo) updateData.tipo = tipo;
-        
+        if (nome) updateData.nome = nome;
+        if (email) updateData.email = email;
+        if (senha) updateData.senha = senha;
+        if (tipo) updateData.tipo = tipo;
+
         const user = await prisma.user.update({
-            where: {id: parseInt(id)},
+            where: { id: parseInt(id) },
             data: updateData
         });
 
@@ -105,39 +103,71 @@ async function updateUser(req,res){
             message: 'Usuario atualiado com sucesso',
             user
         });
-    } catch(error){
+    } catch (error) {
         console.error('Erro ao atualizar o usuario:', error);
 
-        if(error.code === 'P2002'){
-            res.status(400).json({error: 'Email ja esta em uso'})
+        if (error.code === 'P2002') {
+            res.status(400).json({ error: 'Email ja esta em uso' })
         }
-        else{
-            res.status(500).json({error: 'Erro ao atualizar o usuario'});
+        else {
+            res.status(500).json({ error: 'Erro ao atualizar o usuario' });
         }
     }
 }
 
-async function deleteUser(req,res){
-    try{
+async function deleteUser(req, res) {
+    try {
 
-        const {id} = req.params;
+        const { id } = req.params;
 
         const userExists = await prisma.user.findUnique({
-            where: {id: parseInt(id)}
+            where: { id: parseInt(id) }
         });
 
-        if(!userExists){
-            res.status(400).json({error: 'Usuario nao encontrado'})
+        if (!userExists) {
+            res.status(400).json({ error: 'Usuario nao encontrado' })
         }
 
         await prisma.user.delete({
-            where: {id: parseInt(id)}
+            where: { id: parseInt(id) }
         });
 
-        res.json({message: 'Usuario deletado com sucesso'});
+        res.json({ message: 'Usuario deletado com sucesso' });
     }
     catch (error) {
         console.error('Erro ao deletar usuario:', error);
-        res.status(500).json({error: 'Erro ao deletar o usuario'});
+        res.status(500).json({ error: 'Erro ao deletar o usuario' });
     }
 }
+
+async function getUserRecipes(req, res) {
+    try {
+        const { id } = req.params;
+
+        const recipes = await prisma.recipe.findMany({
+            where: { id: parseInt(id) },
+            include: {
+                ingredientes: {
+                    include: {
+                        ingredient: true
+                    }
+                }
+            }
+        });
+
+        res.json(recipes);
+    }
+    catch (error) {
+        console.error('Erro ao buscar receitas:', error);
+        res.status(500).json({ error: 'Erro ao buscar receita' });
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    getUserRecipes
+};
