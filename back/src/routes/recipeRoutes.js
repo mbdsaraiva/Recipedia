370bbd05381ead2f5d1c96dd62c774f2dac6cd1f
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-09-19 20:50:35 -0300

    recipeRoutes.js

---
+++ back/src/routes/recipeRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const recipeController = require('../controllers/recipeController');
+
+router.get('/can-make/:userId', recipeController.getRecipesUserCanMake);
+router.get('/category/:categoria', recipeController.getRecipesByCategory); 
+
+router.get('/', recipeController.getAllRecipes);             
+router.get('/:id', recipeController.getRecipeById);       
+router.post('/', recipeController.createRecipe);                
+router.put('/:id', recipeController.updateRecipe);              
+router.delete('/:id', recipeController.deleteRecipe);           
+
+module.exports = router;
